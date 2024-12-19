1c9901a132a884a44fd73b66dbc04e6894afd014
Jonas Birmé <jonas.birme@eyevinn.se>
2024-12-19 11:18:42 +0100

    fix: invalid config generated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
-echo -e '{\n\
-  "wwwAddress": "0.0.0.0",\n\
-  "wwwPort": 8080,\n\
-  "wwwPublicURL": "https://${OSC_HOSTNAME}",\n\
-  "serviceAddress": "0.0.0.0",\n\
-  "servicePort": 8085,\n\
-  "servicePublicURL": "",\n\
-  "smtpAddress": "0.0.0.0",\n\
-  "smtpPort": 2500,\n\
-  "dbEngine": "SQLite",\n\
-  "dbHost": "",\n\
-  "dbPort": 0,\n\
-  "dbDatabase": "./mailslurper.db",\n\
-  "dbUserName": "",\n\
-  "dbPassword": "",\n\
-  "maxWorkers": 1000,\n\
-  "autoStartBrowser": false,\n\
-  "keyFile": "",\n\
-  "certFile": "",\n\
-  "adminKeyFile": "",\n\
-  "adminCertFile": ""\n\
-  }'\
-  > config.json
 
-  exec "$@"
+PUBLIC_URL=""
+if [ -n "${OSC_HOSTNAME}" ]; then
+  PUBLIC_URL="https://${OSC_HOSTNAME}"
+fi
+
+cat << EOF > config.json
+{
+  "wwwAddress": "0.0.0.0",
+  "wwwPort": 8080,
+  "wwwPublicURL": "${PUBLIC_URL}",
+  "serviceAddress": "0.0.0.0",
+  "servicePort": 8085,
+  "servicePublicURL": "",
+  "smtpAddress": "0.0.0.0",
+  "smtpPort": 2500,
+  "dbEngine": "SQLite",
+  "dbHost": "",
+  "dbPort": 0,
+  "dbDatabase": "./mailslurper.db",
+  "dbUserName": "",
+  "dbPassword": "",
+  "maxWorkers": 1000,
+  "autoStartBrowser": false,
+  "keyFile": "",
+  "certFile": "",
+  "adminKeyFile": "",
+  "adminCertFile": ""
+}
+EOF
+
+exec "$@"
